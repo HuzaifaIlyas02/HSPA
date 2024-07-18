30c0645875695848936b78b5aaef62ed9e903282
Muhammad Huzaifa Ilyas <huzaifailyas02@outlook.com>
2024-07-18 18:11:07 +0500

    Registeration form using Reactive Forms

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -11,12 +11,16 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 const appRoutes: Routes = [
   { path: '', component: PropertyListComponent },
   { path: 'add-property', component: AddPropertyComponent },
   { path: 'rent-property', component: PropertyListComponent },
   { path: 'property-detail/:id', component: PropertyDetailComponent },
+  { path: 'user/login', component: UserLoginComponent },
+  { path: 'user/register', component: UserRegisterComponent },
   { path: '**', component: PropertyListComponent },
 ];
 
@@ -28,12 +32,15 @@ const appRoutes: Routes = [
     PropertyListComponent,
     AddPropertyComponent,
     PropertyDetailComponent,
+    UserRegisterComponent,
+    UserLoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule,
+    FormsModule, // For Template Forms
+    ReactiveFormsModule, // For Reactive Forms
   ],
   providers: [HousingService],
   bootstrap: [AppComponent],
---
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css'],
+})
+export class UserRegisterComponent implements OnInit {
+  registerationForm!: FormGroup;
+  constructor() {}
+
+  ngOnInit() {
+    this.registerationForm = new FormGroup(
+      {
+        userName: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+        ]),
+        confirmPassword: new FormControl('', [Validators.required]),
+        mobile: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(10),
+        ]),
+      },
+      this.passwordMatchingValidator
+    );
+  }
+
+  passwordMatchingValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (
+      password &&
+      confirmPassword &&
+      password.value !== confirmPassword.value
+    ) {
+      return { mismatch: true };
+    }
+
+    return null;
+  }
+
+  get userName() {
+    return this.registerationForm.get('userName') as FormControl;
+  }
+
+  get email() {
+    return this.registerationForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registerationForm.get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.registerationForm.get('confirmPassword') as FormControl;
+  }
+
+  get mobile() {
+    return this.registerationForm.get('mobile') as FormControl;
+  }
+
+  onSubmit() {
+    console.log(this.registerationForm);
+  }
+}
